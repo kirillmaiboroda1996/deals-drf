64d5b02ac64a7f92feb03d36aea4888a6a4acb24
kirillmaiboroda <kirillmaiboroda@gmail.com>
2021-01-22 00:34:16 +0300

    added test for function get_best_five_deals

--- deals_api/deals/tests/test_services.py
+++ deals_api/deals/tests/test_services.py
@@ -4,7 +4,40 @@ from deals.services import (
     _get_common_gems,
 )
 
+from deals.models import Deal
+
 
 class ServiceTestCase(TestCase):
     def test_best_five_deals(self):
-        pass
+        deal1 = Deal.objects.create(
+            customer='test_customer1', item='тест1', quantity=5, total=1500,
+            date='2018-12-14 08:29:52.506166'
+        )
+
+        deal2 = Deal.objects.create(
+            customer='test_customer2', item='тест2', quantity=5, total=1100,
+            date='2018-12-14 08:29:52.506166'
+        )
+
+        deal3 = Deal.objects.create(
+            customer='test_customer3', item='тест3', quantity=5, total=1200,
+            date='2018-12-14 08:29:52.506166'
+        )
+
+        deal4 = Deal.objects.create(
+            customer='test_customer1', item='тест2', quantity=5, total=1100,
+            date='2018-12-14 08:29:52.506166'
+        )
+
+        deal5 = Deal.objects.create(
+            customer='test_customer2', item='тест1', quantity=5, total=1500,
+            date='2018-12-14 08:29:52.506166'
+        )
+
+        data = [{'username': 'test_customer1', 'spent_money': 2600, 'gems': ['тест1', 'тест2']},
+                {'username': 'test_customer2', 'spent_money': 2600, 'gems': ['тест1', 'тест2']},
+                {'username': 'test_customer3', 'spent_money': 1200, 'gems': []}]
+
+        result = get_best_five_deals()
+
+        self.assertEqual(data, result)
